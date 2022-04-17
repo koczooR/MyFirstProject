367eb526d36164cea81141868ea790871eee1a23
koczoooR <koczorowski94@gmail.com>
2022-04-17 19:20:09 +0200

    file paths changes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const btn = document.querySelector(".btn");
 const container = document.querySelector(".container");
-const audio = new Audio("/Assets/Music/pika.mp3");
+const audio = new Audio("Assets/Music/pika.mp3");
 audio.volume = 0.05;
 
 btn.addEventListener("click", function(event) {
     event.preventDefault();
     setTimeout(function() {
-        window.location.href = "/main.html";
+        window.location.href = "main.html";
     }, 500);
     container.style.transition = "1s";
     container.style.opacity = "0";
